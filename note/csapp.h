d2e846b84437d736a18a2155b1b1a0d3837596c8
Your Name <shell-yao@outlook.com>
2018-01-21 02:19:22 -0800

    aaaa

--- note/csapp.h
+++ note/csapp.h
@@ -1,8 +1,16 @@
 #ifndef CSAPP_H_
 #define CSAPP_H_
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 /* Persistent state for the robust I/O (Rio) package */
 /* $begin rio_t */
@@ -25,5 +33,23 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
 
 
 static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n);
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define MAXLINE 8192
+#define LISTENQ  1024 
+#define MAXBUF   8192
+
+typedef struct sockaddr SA;
+int open_clientfd(char *hostname, char *port);
+int open_listenfd(char *port);
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+extern char **environ; /* Defined by libc */
 
 #endif // CSAPP_H_
